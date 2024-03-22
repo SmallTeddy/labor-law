d049aa3220dabf42baa3c84cb2aed78471f2e1ce
SmallTeddy <zhangzeming1212@gmail.com>
2024-03-22 15:27:49 +0800

    feat: add search

--- components/LaborLaw.vue
+++ components/LaborLaw.vue
@@ -1,13 +1,18 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { ItemType } from '../interface';
 import { laborLawItems } from '../law/labor'
 
-const contentItems = ref(laborLawItems)
+const resetItems = () => {
+  return JSON.parse(JSON.stringify(laborLawItems))
+}
+
+const contentItems = ref()
+contentItems.value = resetItems()
 
 const itemClick = (item: ItemType) => {
   let el
-  if(item.value.includes('catalogue')) {
+  if (item.value.includes('catalogue')) {
     el = document.querySelector(`#chapter-${item.value.split('-')[1]}`)
   }
   if (el === null || el === undefined) {
@@ -18,30 +23,46 @@ const itemClick = (item: ItemType) => {
 
 const keyWords = ref('')
 const searchByKeyWords = (keyWords: string) => {
-  const contents = laborLawItems.filter(item => item.content.includes(keyWords))
-  console.log(contents)
+  contentItems.value = resetItems()
+  const contents: ItemType[] = contentItems.value
+    .filter((item: ItemType) => !item.value.includes('catalogue'))
+    .filter((item: ItemType) => item.tier === 'content')
+    .filter((item: ItemType) => item.content.includes(keyWords))
+  contents.forEach((item: ItemType) => {
+    item.content = item.content.replaceAll(keyWords, `<span style="background: yellow; font-weight: bold; padding: 0 2px">${keyWords}</span>`)
+  })
+  contentItems.value = contents
 }
+
+watch(
+  () => keyWords.value,
+  (keyWords) => {
+    if (keyWords && keyWords.trim().length > 0) {
+      searchByKeyWords(keyWords)
+    } else {
+      contentItems.value = resetItems()
+    }
+  }
+)
 </script>
 
 <template>
   <div class="layout">
     <div class="catalogue flex-column flex-align">
-      <h2 id="labor-title">中华人民共和国劳动法</h2>
-      <p>
+      <h2 id="labor-title">《中华人民共和国劳动法》</h2>
+      <p v-if="!keyWords">
         （1994年7月5日第八届全国人民代表大会常务委员会第八次会议通过　根据2009年8月27日第十一届全国人民代表大会常务委员会第十次会议《关于修改部分法律的决定》第一次修正　根据2018年12月29日第十三届全国人民代表大会常务委员会第七次会议《关于修改〈中华人民共和国劳动法〉等七部法律的决定》第二次修正）
       </p>
       <div class="content-items flex-column">
         <div v-for="item in contentItems" :key="item.value" @click="itemClick(item)">
-          <h3 v-if="item.tier === 'title'" :id="item.value" class="flex-c">{{ item.content }}</h3>
-          <p v-if="item.tier === 'content'" :class="{ important: item.important}">{{ item.content }}</p>
+          <h3 v-if="item.tier === 'title'" class="flex-c">{{ item.content }}</h3>
+          <p v-if="item.tier === 'content'" :class="{ important: item.important }" v-html="item.content"></p>
           <br v-if="item.tier === 'br'" />
         </div>
       </div>
     </div>
     <div class="search-box flex flex-align">
       <input type="text" v-model="keyWords" />
-      <button>⬆️</button>
-      <button @click="searchByKeyWords(keyWords)">⬇️</button>
     </div>
   </div>
 </template>
@@ -52,6 +73,7 @@ const searchByKeyWords = (keyWords: string) => {
   padding: 4px 0;
   position: relative;
   z-index: 0;
+  padding-bottom: 30px;
 }
 
 .content-items h3,
@@ -80,24 +102,10 @@ const searchByKeyWords = (keyWords: string) => {
   padding-bottom: 2px;
 }
 
-.search-box input  {
+.search-box input {
+  width: 100%;
   margin: 0;
   padding: 0;
-  margin-right: 4px;
   height: 28px;
-  flex: 1;
-}
-
-.search-box button {
-  margin: 0 4px;
-  background: #fff;
-  border: 1px solid #ccc;
-  height: 32px;
-  line-height: 32px;
-  width: 32px;
-}
-
-.search-box button+button {
-  margin-right: 0;
 }
 </style>
